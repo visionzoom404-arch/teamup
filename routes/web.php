5c2c78f63353e2e216b2acb26b1438bbed11913b
visionzoom404-arch <visionzoom404@gmail.com>
2026-07-04 04:14:32 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TeamController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TeamController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    return view('home');
 });
 
 Route::get('/teams', [TeamController::class, 'index']);
-Route::get('/teams/find', [TeamController::class, 'find']);
-Route::get('/teams/create', [TeamController::class, 'create']);
-Route::post('/teams', [TeamController::class, 'store']);
-
-require __DIR__.'/auth.php';
